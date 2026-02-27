3f5b9a506130e390fae8e4e81937e18201adb462
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2026-02-27 20:47:31 +0500

    update

--- backend/models/Maintenance.js
+++ backend/models/Maintenance.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 const maintenanceSchema = new mongoose.Schema(
   {
     room: { type: mongoose.Schema.Types.ObjectId, ref: "Room" },
+    reportedBy:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
     issue: String,
     status: {
       type: String,
-      enum: ["pending", "resolved"],
+      enum: ["pending", "resolved", "in-progress"],
       default: "pending",
     },
     reportedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,12 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const authRoutes = require("./routes/authRoutes.js");
+const mongoose = require("mongoose");
 
+const app = express();
 dotenv.config();
 connectDB();
 
-const app = express();
 
 app.use(express.json());
 app.use(cors());
